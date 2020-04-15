812d4f1a49196ab5e90e1891b349c12ebc389f81
deddysurya <61810610+deddysurya@users.noreply.github.com>
2020-04-14 21:33:49 -0700

    Ujian Tengah Semester
    Program mengkonversi suhu

---
+++ Program Konversi Suhu/Index.js
@@ -0,0 +1,81 @@
+const electron = require("electron");
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let homeWindow;
+let todayWindow;
+let aboutWindow;
+
+let allAppointment = [];
+
+app.on("ready",  () => {
+    homeWindow = new BrowserWindow({
+       webPreferences : {
+           nodeIntegration : true
+        },
+        title : "Aplikasi Temperature"  
+    });
+
+    homeWindow.loadURL(`file://${__dirname}/home.html`);
+    homeWindow.on("closed", () => {
+        app.quit();
+        homeWindow = null;
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+const todayWindowCreator = () => {
+    todayWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Konversi Suhu"
+    });
+
+    todayWindow.setMenu(null);
+    todayWindow.loadURL(`file://${__dirname}/today.html`);
+    todayWindow.on("closed", ()  => (todaytWindow = null));
+};
+const aboutWindowCreator = () => {
+    aboutWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "About"
+    });
+
+    aboutWindow.setMenu(null);
+    aboutWindow.loadURL(`File://${__dirname}/about.html`);
+    aboutWindow.on("closed", ()  => (aboutWindow = null));
+};
+
+const menuTemplate = 
+[
+    {
+        label : "Konversi",
+        click(){
+        todayWindowCreator();
+        }
+    },
+    {
+        label:"About",
+        click()
+        {
+            aboutWindowCreator();
+        }
+    },
+    {
+        label: "Quit",
+        accelerato : process.platform === "darwin" ? "Command+Q" :
+        "Ctrl+Q",
+        click(){
+        app.quit(); 
+        }
+    },
+];
